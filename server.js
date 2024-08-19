25fe047b6d1cc18b1b49ad6c5acbffb000a6afce
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-19 17:10:24 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+// The callback function runs everytime a request is made
+const server = http.createServer((req, res) => {
+  console.log("Request made");
+});
+
+// takes the port and hostname and a callback function tht at runs when we start listening
+server.listen(3000, "localhost", () => {
+  console.log("Listening on port 3000");
+});
+
+// localhost is a domain name that takes us to a loopback address i.e back to our own computer for requests.
+
+// port number represents a specific gate, they are like doors in a computer.
+
