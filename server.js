0a9541e3de82510bdec98fbd8c25c2d0b9322e53
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-19 17:13:24 +0200

    feat: counting requests made

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require("http");
 
+let count = 0;
+
 // The callback function runs everytime a request is made
 const server = http.createServer((req, res) => {
   console.log("Request made");
+  count += 1;
+  console.log(count);
 });
 
 // takes the port and hostname and a callback function tht at runs when we start listening
@@ -13,4 +17,3 @@ server.listen(3000, "localhost", () => {
 // localhost is a domain name that takes us to a loopback address i.e back to our own computer for requests.
 
 // port number represents a specific gate, they are like doors in a computer.
-
