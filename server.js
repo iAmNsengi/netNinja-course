fb51dc509c3d2b4294ea9f5cd4ac91da2ca9fb6d
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-20 12:45:05 +0200

    feat: initialized the project

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 
 let count = 0;
 
 // The callback function runs everytime a request is made
 const server = http.createServer((req, res) => {
   console.log("Request made");
-  count += 1;
-  console.log(count);
+
+  // set header content type
+  //   res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/html");
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+
+  //   res.write("Hello, Nsengi");
+  // res.write("<p>I am Nsengi</p>");
 });
 
 // takes the port and hostname and a callback function tht at runs when we start listening
-server.listen(3000, "localhost", () => {
+server.listen(3001, "localhost", () => {
   console.log("Listening on port 3000");
 });
 
