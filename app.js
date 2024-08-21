e4267374d2272a9da815139a9ac209f24e1bd89d
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-21 14:22:49 +0200

    feat: implementing the ejs

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const express = require("express");
 // express app
 const app = express();
 
-
+// registering the view engine
+app.set("view engine", "ejs");
 
 // listening for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
+  //   res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.get("/about", (req, res) => {
